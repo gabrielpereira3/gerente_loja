4862bcd42a043afd3d08cefdddac9219f3fcc826
Gabriel <gabriel.soares14563@gmail.com>
2022-06-15 09:55:26 -0300

    Validação dos campos de email e senha, centralização do conteúdo

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:gerente_loja/blocs/login_bloc.dart';
 import 'package:gerente_loja/widgets/input_field.dart';
 
 class LoginScreen extends StatefulWidget {
@@ -9,35 +10,66 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  final _loginBloc = LoginBloc();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[850],
-      body: SingleChildScrollView(
-        child: Container(
-          margin: const EdgeInsets.all(16),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const Icon(
-                Icons.store_mall_directory,
-                color: Colors.pinkAccent,
-                size: 160,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          SingleChildScrollView(
+            child: Container(
+              margin: const EdgeInsets.all(16),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  const Icon(
+                    Icons.store_mall_directory,
+                    color: Colors.pinkAccent,
+                    size: 160,
+                  ),
+                  InputField(
+                    Icons.person_outline,
+                    "Usuário",
+                    false,
+                    _loginBloc.outEmail,
+                    _loginBloc.changedEmail,
+                  ),
+                  InputField(
+                    Icons.lock_outline,
+                    "Senha",
+                    true,
+                    _loginBloc.outPassword,
+                    _loginBloc.changedPassword,
+                  ),
+                  const SizedBox(height: 32),
+                  StreamBuilder<bool>(
+                      stream: _loginBloc.outSubmitValid,
+                      builder: (context, snapshot) {
+                        return SizedBox(
+                          height: 45,
+                          child: TextButton(
+                            onPressed: snapshot.hasData ? () {} : null,
+                            style: ButtonStyle(
+                                backgroundColor: snapshot.hasData
+                                    ? MaterialStateProperty.all(
+                                        Colors.pinkAccent)
+                                    : MaterialStateProperty.all(Colors.pinkAccent.withAlpha(140))),
+                            child: const Text(
+                              "Entrar",
+                              style:
+                                  TextStyle(color: Colors.white, fontSize: 17),
+                            ),
+                          ),
+                        );
+                      }),
+                ],
               ),
-              const InputField(Icons.person_outline, "Usuário", false),
-              const InputField(Icons.lock_outline, "Senha", true),
-              TextButton(
-                onPressed: () {},
-                style: ButtonStyle(backgroundColor: MaterialStateProperty.all(Colors.pinkAccent)),
-                child: const Text(
-                  "Entrar",
-                  style: TextStyle(color: Colors.white),
-                ),
-              ),
-            ],
+            ),
           ),
-        ),
+        ],
       ),
     );
   }
--- lib/widgets/input_field.dart
+++ lib/widgets/input_field.dart
@@ -1,29 +1,39 @@
 import 'package:flutter/material.dart';
 
 class InputField extends StatelessWidget {
-  const InputField(this.icon, this.hint, this.obscure, {Key? key})
+  const InputField(this.icon, this.hint, this.obscure, this.stream, this.onChanged, {Key? key})
       : super(key: key);
 
   final IconData icon;
   final String hint;
   final bool obscure;
+  final Stream<String> stream;
+  final Function(String) onChanged;
 
   @override
   Widget build(BuildContext context) {
-    return TextField(
-      decoration: InputDecoration(
-        icon: Icon(
-          icon,
-          color: Colors.white,
-        ),
-        hintText: hint,
-        hintStyle: const TextStyle(color: Colors.white),
-        focusedBorder: const UnderlineInputBorder(
-          borderSide: BorderSide(color: Colors.pinkAccent),
-        ),
-      ),
-      style: const  TextStyle(color: Colors.white),
-      obscureText: obscure,
+    return StreamBuilder<String>(
+      stream: stream,
+      builder: (context, snapshot) {
+        return TextField(
+          onChanged: onChanged,
+          decoration: InputDecoration(
+            icon: Icon(
+              icon,
+              color: Colors.white,
+            ),
+            hintText: hint,
+            hintStyle: const TextStyle(color: Colors.white),
+            focusedBorder: const UnderlineInputBorder(
+              borderSide: BorderSide(color: Colors.pinkAccent),
+            ),
+            contentPadding: const EdgeInsets.only(left: 5, right: 20, top: 20, bottom: 20),
+            errorText: snapshot.hasError ? snapshot.error.toString() : null,
+          ),
+          style: const TextStyle(color: Colors.white),
+          obscureText: obscure,
+        );
+      }
     );
   }
 }
