bbbbcb4c788c907a740dbe5a6fc29367fa732052
Gabriel <gabriel.soares14563@gmail.com>
2022-06-20 10:12:28 -0300

    Shimmer produzindo uma animação enquanto os dados são carregados

--- lib/widgets/user_tile.dart
+++ lib/widgets/user_tile.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shimmer/shimmer.dart';
 
 class UserTile extends StatelessWidget {
   const UserTile(this.user, {Key? key}) : super(key: key);
@@ -9,8 +10,7 @@ class UserTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    
-    if(user.containsKey("money")){
+    if (user.containsKey("money")) {
       return ListTile(
         title: Text(user["name"], style: textStyle),
         subtitle: Text(user["email"], style: textStyle),
@@ -18,12 +18,44 @@ class UserTile extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.end,
           children: [
             Text("Pedidos: ${user["orders"]}", style: textStyle),
-            Text("Gastos: R\$${user["money"].toStringAsFixed(2)}", style: textStyle),
+            Text("Gastos: R\$${user["money"].toStringAsFixed(2)}",
+                style: textStyle),
           ],
         ),
       );
     } else {
-      return Container();
+      return Container(
+        margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(
+              width: 200,
+              height: 20,
+              child: Shimmer.fromColors(
+                baseColor: Colors.white,
+                highlightColor: Colors.grey,
+                child: Container(
+                  color: Colors.white.withAlpha(50),
+                  margin: const EdgeInsets.symmetric(vertical: 4),
+                ),
+              ),
+            ),
+            SizedBox(
+              width: 50,
+              height: 20,
+              child: Shimmer.fromColors(
+                baseColor: Colors.white,
+                highlightColor: Colors.grey,
+                child: Container(
+                  color: Colors.white.withAlpha(50),
+                  margin: const EdgeInsets.symmetric(vertical: 4),
+                ),
+              ),
+            ),
+          ],
+        ),
+      );
     }
   }
 }
